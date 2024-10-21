e83d287bcf87d1adbbde9039740eba55b83b1799
talivo <76170849+talivo@users.noreply.github.com>
2024-10-21 19:46:15 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿decimal energyPrice = 0.03M;
+int energyCurrent = 0;
+
+Console.WriteLine("Start car charge");
+
+Console.WriteLine("Wait on charging until cost is <= Eur 0,12");
+
+switch(energyPrice) {
+    case <= 0.05M:
+        energyCurrent = 11000;
+        break;
+    case 0.06M:
+        energyCurrent = 10000;
+        break;
+    case 0.07M:
+        energyCurrent = 9000;
+        break;
+    case 0.08M:
+        energyCurrent = 8000;
+        break;
+    case 0.09M:
+        energyCurrent = 7500;
+        break;
+    case 0.10M:
+        energyCurrent = 7000;
+        break;
+    case 0.11M:
+        energyCurrent = 6000;
+        break;
+    case >= 0.12M:
+        energyCurrent = 5000;
+        break;
+}
